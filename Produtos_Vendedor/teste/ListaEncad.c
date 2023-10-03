11cc3a961f2cdab31619e4efdcb01da33476ba7a
Lucas Araújo de Oliveira <127609884+lucasaraujo20@users.noreply.github.com>
2023-10-03 17:39:46 -0300

    Add files via upload

---
+++ Produtos_Vendedor/teste/ListaEncad.c
@@ -0,0 +1,112 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "ListaEncad.h"
+
+lista_vendedores *criar_lista_vendedores()
+{
+    lista_vendedores *l = (lista_vendedores *)malloc(sizeof(lista_vendedores));
+    l->inicio = NULL;
+    l->total_vendedores = 0;
+    return l;
+}
+
+int lista_vendedores_vazia(lista_vendedores *l)
+{
+    if (l == NULL)
+        return 1;
+    if (l->inicio == NULL && l->total_vendedores == 0)
+        return 1;
+    else
+        return 0;
+}
+
+int verifica_vendedor(lista_vendedores *l, cadastro it, vendedor v)
+{
+    if (l == NULL)
+        return 1;
+    if (lista_vendedores_vazia(l) == 0)
+        return insere_novo_vendedor(l, v);
+    no_vendedores *atual = l->inicio;
+    while (atual != NULL)
+    {
+        if (strcmp(atual->valor.cadastro.nome, it.nome) == 0)
+        {
+            return 1;
+        }
+        atual = atual->prox;
+    }
+    return 0;
+}
+
+int insere_novo_vendedor(lista_vendedores *l, vendedor v)
+{
+    if (l == NULL)
+    {
+        return 1;
+    }
+
+    no_vendedores *novo_no = (no_vendedores *)malloc(sizeof(no_vendedores));
+    if (novo_no == NULL)
+    {
+        return 0;
+    }
+
+    novo_no->valor = v;
+
+    novo_no->prox = l->inicio;
+    novo_no->ant = NULL;
+
+    if (l->inicio != NULL)
+    {
+
+        l->inicio->ant = novo_no;
+    }
+
+    l->inicio = novo_no;
+
+    l->total_vendedores++;
+    return 2;
+}
+
+void libera_lista_vendedores(lista_vendedores *l)
+{
+    no_vendedores *atual = l->inicio;
+    while (atual != NULL)
+    {
+        no_vendedores *temp = atual;
+        atual = atual->prox;
+        free(temp);
+    }
+    l->inicio = NULL;
+    l->total_vendedores = 0;
+}
+
+void mostrar_lista_vendedores(lista_vendedores *l)
+{
+    if (l == NULL)
+    {
+        printf("Lista de vendedores nao criada!\n");
+        return;
+    }
+    if (lista_vendedores_vazia(l) == 1)
+    {
+        printf("Lista de vendedores vazia!\n");
+        return;
+    }
+
+    no_vendedores *search = l->inicio;
+    int cont = 1;
+
+    while (search != NULL)
+    {
+        printf("Vendedor %d:\n", cont);
+        printf("Nome: %s\n", search->valor.cadastro.nome);
+        printf("NomeLoja: %s\n", search->valor.nomeloja);
+        printf("Senha: %s\n", search->valor.cadastro.senha);
+        printf("\n");
+
+        search = search->prox;
+        cont++;
+    }
+}
---
+++ Produtos_Vendedor/teste/ListaEncad.h
@@ -0,0 +1,47 @@
+#ifndef LISTAENCAD_H
+#define LISTAENCAD_H
+
+typedef struct cadastro {
+    char nome[30];
+    char senha[10];  
+} cadastro;
+
+typedef struct produtos {
+    int CATEGORIA;
+    char NOME[30];
+    char DESCRICAO[100];
+    float VALOR;
+    int QUANTIDADE;
+    int NOTA_AVALIACAO;
+} produtos;
+
+typedef struct vendedor{
+    char nomeloja[30];
+    cadastro cadastro;
+}vendedor;
+
+typedef struct no_vendedores {
+    vendedor valor;
+    struct no_vendedores *prox;
+    struct no_vendedores *ant;
+} no_vendedores;
+
+typedef struct lista_vendedores {
+    int total_vendedores;
+    no_vendedores *inicio;
+} lista_vendedores;
+
+
+
+
+lista_vendedores *criar_lista_vendedores();
+
+int lista_vendedores_vazia(lista_vendedores *l);
+
+int verifica_vendedor(lista_vendedores *l,cadastro it, vendedor v);
+
+int insere_novo_vendedor(lista_vendedores *l,vendedor v);
+
+void mostrar_lista_vendedores(lista_vendedores *l);
+
+#endif
