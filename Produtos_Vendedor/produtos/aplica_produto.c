2c05cd8f3c441ab9b0134f2f5dad217252a82803
Breno Oliveira Cavalcante <breno.cavalcante@ufu.br>
2023-10-12 11:58:16 -0300

    comeco do produto

---
+++ Produtos_Vendedor/produtos/aplica_produto.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lista_produto.h"
+
+/* --  A lista de produtos será implementada usando Lista duplamente encadeada
+       pela facilidade de se adicionar e remover produtos, e pela maior facilidade
+       de manutenção do código e implementação de novas funçoes.
+   --
+*/
+
---
+++ Produtos_Vendedor/produtos/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "aplica_produto.c"
+
+int main(){
+    int x;
+    
+    return 0;
+}
